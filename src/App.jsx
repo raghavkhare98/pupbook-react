d0aa5eda404ca7b63953191aa438eefccc661d23
raghavkhare98 <khare08rma@gmail.com>
2024-10-05 18:21:52 -0400

    Commit whole repo with changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 //creating a component here
 
 import Header from "./components/Header";
+import RandomInfo from "./components/RandomInfo";
 
 const App = () =>{
-
-  const randomInfoDivStyle = {
-    textAlign: "center",
-    marginTop: "48px", 
-    fontSize: "36px",
-    fontWeight: "bolder",
-    color: "#1F2937"
-  }
 
   const greyDivStyle = {
     marginTop: "180px", 
@@ -55,27 +48,7 @@ const App = () =>{
   return (
     <>
     <Header />
-    <div className="random-info-div" style={{padding: "8px 120px"}}>
-        <p style={randomInfoDivStyle}>Current Pupbook users.</p>
-        <div className="cards">
-            <div className="card">
-                <img src="./resources/pup-1.jpg" alt="pupper 1" width="200px" height="200px" style={{borderRadius: "12px"}}/>
-                <p style={{textAlign: "center"}}>Hi! My name is Pupper and I'm a corgi</p>
-            </div>    
-            <div className="card">
-                <img src="./resources/pup-2.jpg" alt="pupper 2" width="200px" height="200px" style={{borderRadius: "12px"}}/>
-                <p style={{textAlign: "center"}}>Hi! My name is Upper and I'm a smol bean</p>
-            </div>    
-            <div className="card">
-                <img src="./resources/pup-3.jpg" alt="pupper 3" width="200px" height="200px" style={{borderRadius: "12px"}}/>
-                <p style={{textAlign: "center"}}>Hi! My name is doge and I'm a Shiba Inu</p>
-            </div>
-            <div className="card">
-                <img src="./resources/pup-4.jpg" alt="pupper 4" width="200px" height="200px" style={{borderRadius: "12px"}}/>
-                <p style={{textAlign: "center"}}>Hi! My name is Windy and I'm a pomeranian</p>
-            </div>        
-        </div>
-    </div>
+    <RandomInfo />
     <div className="grey-div">
         <p style={greyDivStyle}>
             I love using Pupbook! All my friends use Pupbook and invite me for parties using pupbook! It's also a place where us dogs can bitch(hehe) about our owners.
